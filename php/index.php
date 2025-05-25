8a62343b710e669fa1a2881b027ba2c99ee839ed
DollySarmiento2024 <dosaqu@alumnatflorida.es>
2025-05-25 14:43:43 +0200

    cambios en acceso bbdd y mysql

--- php/index.php
+++ php/index.php
@@ -1,8 +1,8 @@
 <?php
 // Conexión a la base de datos
 $servername = "db";
-$username = "fernanda";//"root";
-$password = "fernanda";//"";
+$username = "root";//"root";
+$password = "root";//"";
 $dbname = "concesionario";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
