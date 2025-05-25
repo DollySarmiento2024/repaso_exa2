6689caa31776b89e863192a59f58c189a8453dbc
DollySarmiento2024 <dosaqu@alumnatflorida.es>
2025-05-26 00:34:59 +0200

    cambio menor

--- php/index.php
+++ php/index.php
@@ -7,7 +7,7 @@ $dbname = "concesionario";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
+if ($conn->connect_error) { 
     die("Conexión fallida: " . $conn->connect_error);
 }
 
